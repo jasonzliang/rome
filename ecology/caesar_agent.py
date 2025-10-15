ca80c0f863b77989e07d0acecaaeadbfa32932bc
Jason L <jasonzliang@utexas.edu>
2025-10-15 03:03:59 -0700

    latest

--- ecology/caesar_agent.py
+++ ecology/caesar_agent.py
@@ -425,8 +425,9 @@ Provide 3-5 concise, substantive insights that are roughly 250-500 tokens in len
         for i, (url, text) in enumerate(links):
             # Get the visit count from the self.visited_urls dictionary, defaulting to 0.
             visit_count = self.visited_urls.get(url, 0)
+            visit_info = f" ({visit_count} visits so far) " if visit_count > 0 else " "
             link_options.append(
-                f"{i+1}. [{text}] (visit count: {visit_count}) {url}"
+                f"{i+1}. [{text}]{visit_info}{url}"
             )
         link_options = '\n'.join(link_options[:MAX_NUM_LINKS])
 
