a0dbc2fe437f5adb3abc628eb55a9fecd3fe463e
Jason L <jasonzliang@utexas.edu>
2025-11-17 04:02:00 -0800

    latest

--- ecology/caesar_config.py
+++ ecology/caesar_config.py
@@ -4,10 +4,6 @@ MAX_NUM_LINKS = 2000
 MAX_NUM_VISITED_LINKS = 1000
 # Maximum of neighboring nodes to look for related insights
 MAX_NUM_NEIGHBORS = 5
-# Maximum number of citation sources per query during synthesis
-MAX_SYNTHESIS_QUERY_SOURCES = 5
-# Max QA pairs to keep in context during iterative synthesis
-MAX_SYNTHESIS_QA_CONTEXT = 50
 # Maximum length (characters) of text to extract from a webpage
 MAX_TEXT_LENGTH = 100000
 # Timeout for fetching webpage html using requests
@@ -15,22 +11,22 @@ REQUESTS_TIMEOUT = 10
 # Headers to use for requests when fetching html
 REQUESTS_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Referer': 'https://www.google.com/',
-    'DNT': '1',
-    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip, deflate',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-Site': 'none',
     'Sec-Fetch-User': '?1',
     'Sec-Ch-Ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
     'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"Windows"',
-    'Cache-Control': 'max-age=0',
+    'Sec-Ch-Ua-Platform': '"Windows"'
 }
+# Maximum number of citation sources per query during synthesis
+MAX_SYNTHESIS_QUERY_SOURCES = 5
+# Max QA pairs to keep in context during iterative synthesis
+MAX_SYNTHESIS_QA_CONTEXT = 50
 
 CAESAR_CONFIG = {
     "CaesarAgent": {
