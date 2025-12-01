c355648cb1b75337c278b39bcfab9c84f5499143
jason.liang <jason.liang@cognizant.com>
2025-12-01 11:36:29 -0800

    latest

--- ecology/caesar_config.py
+++ ecology/caesar_config.py
@@ -31,7 +31,7 @@ MAX_SYNTHESIS_QUERY_SOURCES = 5
 MAX_SYNTHESIS_QA_CONTEXT = 50
 # Number of retries for artifact synthesis before failing
 NUM_SYNTHESIS_RETRIES = 5
-# Whether to save JSON for synthesis results as well
+# Whether to save JSON for synthesis artifact as well
 SYNTHESIS_SAVE_JSON = False
 
 CAESAR_CONFIG = {
