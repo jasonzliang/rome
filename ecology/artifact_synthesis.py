f22328b47bb7df6fd8afe4b8c71e48b76b52b3a1
Jason L <jasonzliang@utexas.edu>
2025-12-15 17:32:49 -0800

    latest'

--- ecology/artifact_synthesis.py
+++ ecology/artifact_synthesis.py
@@ -276,6 +276,7 @@ MERGED ARTIFACT CITATIONS:
 
 MERGED ARTIFACT TEXT:
     - IMPORTANT: Avoid excessive jargon, ensure artifact text is well-organized (logical, clear, focused), and convincing to a skeptical reader
+    - IMPORTANT: The merged artifact should be at least as long as each individual round artifact
     - Use your role as a guide on how to respond{query_role}
 
 RESPONSE INSTRUCTIONS:
