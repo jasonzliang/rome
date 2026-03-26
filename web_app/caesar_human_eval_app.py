56ecf0d2ab904a4ffa5eedba6d28713993e1e2d5
Jason L <jasonzliang@utexas.edu>
2026-03-25 19:20:22 -0700

    latest

--- web_app/caesar_human_eval_app.py
+++ web_app/caesar_human_eval_app.py
@@ -122,7 +122,7 @@ with st.sidebar:
             st.markdown("---")
             st.error("🛠️ Admin Panel")
             # st.write("Wipe the entire database (all users).")
-            confirm_wipe = st.checkbox("I confirm I want to wipe EVERYTHING")
+            confirm_wipe = st.checkbox("I want to wipe EVERYTHING")
             if st.button("🔥 DELETE ALL RECORDS", disabled=not confirm_wipe):
                 delete_all_records()
                 st.session_state.user_name = ""
